64001522d5e00946c00625f867d767432a9f9e0a
vniv <valentinnivesse40@gmail.com>
2025-03-16 18:49:16 +0100

    GDA-7 : changement categorie projets

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -88,11 +88,11 @@ export class PagesComponent implements OnInit {
   selectedCategory = 'Tous';
 
   projects = [
-    { id: 2,title: 'Application mobile Blue Ice', overlayText: 'Description du projet 1', category: 'Web', image: 'assets/projects/blueice.mp4', link: '#' },
-    { id: 1,title: 'Application web Link&Trade', overlayText: 'Description du projet 1', category: 'Design', image: 'assets/projects/link&trade.mp4', link: '#' },
-    { id: 5,title: 'Application Windows Simpl-E', overlayText: 'Description du projet 1', category: 'Prototype Figma', image: 'assets/projects/simple.mp4', link: '#' },
-    { id: 3,title: 'Projet IT Grand Angle', overlayText: 'Description du projet 1', category: 'Web', image: 'assets/projects/grandangle.mp4', link: '#' },
-    { id: 4,title: 'Projet IT En cours', overlayText: 'Description du projet 1', category: 'Web', image: 'assets/projects/perso.mp4', link: '#' }
+    { id: 2,title: 'Application mobile Blue Ice', overlayText: 'Description du projet 1', category: 'Formation', image: 'assets/projects/blueice.mp4', link: '#' },
+    { id: 1,title: 'Application web Link&Trade', overlayText: 'Description du projet 1', category: 'Formation', image: 'assets/projects/link&trade.mp4', link: '#' },
+    { id: 5,title: 'Application Windows Simpl-E', overlayText: 'Description du projet 1', category: 'Formation', image: 'assets/projects/simple.mp4', link: '#' },
+    { id: 3,title: 'Projet IT Grand Angle', overlayText: 'Description du projet 1', category: 'Entreprise', image: 'assets/projects/grandangle.mp4', link: '#' },
+    { id: 4,title: 'Projet IT En cours', overlayText: 'Description du projet 1', category: 'Entreprise', image: 'assets/projects/perso.mp4', link: '#' }
   ];
 
   get filteredProjects() {
